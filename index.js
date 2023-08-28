110e851a5029d30e3f5bef596a049de72a567774
Gustavo Castanheira <gustavocastanha@hotmail.com>
2023-08-28 13:36:06 -0300

    api

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-
-const port = 3008;
-let path = require('path');
-const e = require('express');
 const app = express();
+const cors = require('cors');
 
-const login = 'admin'
-const password = 'admin'
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(session({
-    secret: 'mysecret',
-    resave: false, // Define a opção resave como false
-    saveUninitialized: true 
-}));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+const users = [
+  {
+    username: 'senha',
+    password: 'senha'
+  }
+];
 
-    app.post('/', (req, res) => {
-        
-        if (req.body.login == login && req.body.password == password) {
-            //logou
-            req.session.login = login;
-            res.render('logado')
-            console.log('Logado com sucesso')
-         }else{
-            console.log('Login ou senha incorretos')
-             res.render('index')
-         }
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log('Request body:', req.body);
+  const user = users.find(u => u.username === username && u.password === password);
+  console.log('User found:', user);
+  if (user) {
+    res.json({ success: true });
+  } else {
+    res.status(401).json({ success: false });
+  }
+});
 
-    })
-    
-
-    app.get('/', (req, res) => {
-        if (req.session.login) {
-            res.render('logado')
-        }else{
-            res.render('index')
-        }
-        
-    
-    })
-
- app.listen(port, () => {
-        console.log(`Server running on port: ${port}`);
- });
+app.listen(3008, () => console.log('3008'));
